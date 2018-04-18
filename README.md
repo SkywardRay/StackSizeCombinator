5085080df3b0d81b40a0883c5d14c12dadc653df
KuroiRoy <roy.zwart@gmail.com>
2018-04-18 09:37:28 +0200

    Added Readme text The combinator now automatically uses the first item signal it finds on a red/green input wire when the input is set to Each

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # StackSizeCombinator
+
+A combinator that takes an item signal as input and multiplies it by the stack size of that item.
+
+## Usage
+
+Connect a circuit network wire to the input side of the combinator and select the item you want to get the stack size of in the combinator gui, the combinator will fill in the rest by itself.
+
+Selecting Each as the input signal will make the combinator select the first item signal it finds from the circuit network. As far as i know it is not possible to pass a different stack size value to every individual signal using Each as input. This seems like a good compromise because you can set the Each signal in a blueprint and the combinator will automatically pick the first signal it receives.
+
+## Example usage
+
+I like to have my train station only enable itself when there are enough resources to make the trip worth it. Normally I have to set a value manually since I like to set it at around 75% of the maximum capacity. But the chests at the station have a different maximum capacity for every resource they contain due to the difference in stack size. With the Stack Size Combinator I can calculate the maximum and I can use the same blueprint for every train station that offers a resource.
+
+## Known Issues
+
+- The GUI does not update when the filters change while it is open. It is using the standard arithmetic combinator GUI and as far as I know there is no way for a mod to force a GUI to update/close. When you close and reopen the GUI it shows the correct values though
--- control.lua
+++ control.lua
@@ -1,51 +1,103 @@
-local function process_signal(signal, control)
-    if not signal or signal.signal.type ~= "item" then
-        return false
+local function get_first_item_signal(control)
+    local connector_id = defines.circuit_connector_id.combinator_input
+
+    local green_network = control.get_circuit_network(defines.wire_type.green, connector_id)
+
+    -- Try to find an item signal in the green network
+    if green_network and green_network.signals then
+        for j, signal in pairs(green_network.signals) do
+            -- Only return valid item signals
+            -- Using signal.signal because circuit networks carry signals like this
+            -- signal = {signal = {name = "", type = ""}, count = 0}
+            if signal ~= nil and signal.signal ~= nil and signal.signal.name ~= nil and signal.signal.type == "item" then
+                return signal.signal
+            end
+        end
     end
 
-    local stack_size = game.item_prototypes[signal.signal.name].stack_size
+    local red_network = control.get_circuit_network(defines.wire_type.red, connector_id)
 
+    -- Try to find an item signal in the red network
+    if red_network and red_network.signals then
+        for j, signal in pairs(red_network.signals) do
+            -- Only return valid item signals
+            if signal ~= nil and signal.signal ~= nil and signal.signal.name ~= nil and signal.signal.type == "item" then
+                return signal.signal
+            end
+        end
+    end
+
+    -- No item signal was found
+    return nil
+end
+
+local function get_signal(control)
+    -- Make sure the control parameters exist
+    if not control.parameters or not control.parameters.parameters then
+        set_parameters(control, nil, nil, 0)
+    end
+
+    -- Return the input signal
+    return control.parameters.parameters.first_signal
+end
+
+local function set_parameters(control, first_signal, output_signal, stack_size)
     control.parameters = {
         parameters = {
-            first_signal = signal.signal,
+            first_signal = first_signal,
             first_constant = 0,
             operation = "*",
             second_signal = nil,
             second_constant = stack_size,
-            output_signal = signal.signal
+            output_signal = output_signal
         }
     }
-
-    return true
 end
 
-local function on_tick(e)
-    local surface = game.surfaces[1]
-    local combinators = surface.find_entities_filtered({name = "stack-size-combinator"})
+local function update_control(control)
+    if not control then
+        return
+    end
 
-    for i, combinator in pairs(combinators) do
-        local control = combinator.get_control_behavior()
-        local connector_id = defines.circuit_connector_id.combinator_input
+    -- Get the signal set as the input of the combinator
+    local signal = get_signal(control)
 
-        local green_network = control.get_circuit_network(defines.wire_type.green, connector_id)
-        local red_network = control.get_circuit_network(defines.wire_type.red, connector_id)
+    -- Only items have stack sizes, so remove the stack size parameters to let the user know this item has no stack size
+    if signal == nil or signal.name == nil or (signal.name ~= "signal-each" and signal.type ~= "item") then
+        set_parameters(control, signal, nil, 0)
 
-        if green_network and green_network.signals then
-            for j, signal in pairs(green_network.signals) do
-                if process_signal(signal, control) then
-                    return
-                end
-            end
-        end
+        return
+    end
 
-        if red_network and red_network.signals then
-            for j, signal in pairs(red_network.signals) do
-                if process_signal(signal, control) then
-                    return
-                end
-            end
+    -- If the user selects signal-each then we want to set the first item signal we find
+    -- because we unfortunately cannot output the stack size for every item recieved from the network
+    if signal.name == "signal-each" then
+        signal = get_first_item_signal(control) or signal
+    end
+
+    if signal == nil or signal.type ~= "item" then
+        set_parameters(control, signal, nil, 0)
+
+        return
+    end
+
+    -- Get the stack size from the item prototype
+    local stack_size = game.item_prototypes[signal.name].stack_size
+
+    -- Set the control parameters
+    set_parameters(control, signal, signal, stack_size)
+end
+
+local function on_nth_tick(e)
+    for i, surface in pairs(game.surfaces) do
+        -- Get all stack size combinators on this surface
+        local combinators = surface.find_entities_filtered({name = "stack-size-combinator"})
+
+        for j, combinator in pairs(combinators) do
+            update_control(combinator.get_control_behavior())
         end
     end
 end
 
-script.on_nth_tick(300, on_tick)
+-- Update the combinators every 2 seconds (assuming 60 ups)
+script.on_nth_tick(120, on_nth_tick)
